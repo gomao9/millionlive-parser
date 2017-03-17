35e4355fff4229c0b861d2ca0bb69551c02b25ac
gomao <gomaoq@gmail.com>
2017-03-17 17:53:33 +0900

    update ranking scraper

--- lib/millionlive/parser/client.rb
+++ lib/millionlive/parser/client.rb
@@ -4,6 +4,7 @@ module Millionlive::Parser
   class Client
   LOGIN = 'https://id.gree.net/login/entry'
   RANKING_PER_PAGE = 10
+  RANKING_URL = "http://imas.gree-apps.net/app/index.php/event/%s/ranking/general?page=%s&idol=%s"
 
     def initialize email, passwd
       # agent setting
@@ -19,27 +20,26 @@ module Millionlive::Parser
       @agent.page.form.submit
     end
 
-    # get event ranking
-    def event_ranking(event_id, upto=2000, idol_no=nil)
-      (1..Float::INFINITY).each.lazy.flat_map do |index|
-        url = "http://imas.gree-apps.net/app/index.php/event/#{event_id}/ranking/general?page=#{index}&idol=#{idol_no}"
-        if index == 1
-          visit url
-        else
-          @agent.get url
-        end
+    def event_ranking_pages(event_id, page_nos, idol_no=nil)
+      # ランキングページを一度visitしておかないとあとの処理ができない
+      visit format(RANKING_URL, event_id, 1, idol_no)
+      page_nos.flat_map do |page_no|
+        event_ranking_page(event_id, page_no, idol_no)
+      end
+    end
 
-        css = '#wrapper td.user-list-st'
-        tds = page.search(css)
-        tds.map do |td|
-          {
-            rank: td.children[0].text.to_i,
-            name: td.children[3].text,
-            user_id: td.children[3].attr('href').split('/').last.to_i,
-            point: td.children[7].text.delete(',').to_i,
-          }
-        end
-      end.take_while{|h| h[:rank] <= upto}.to_a
+    def event_ranking_page(event_id, page_no, idol_no=nil)
+      @agent.get format(RANKING_URL, event_id, page_no, idol_no)
+      css = '#wrapper td.user-list-st'
+      tds = page.search(css)
+      tds.map do |td|
+        {
+          rank: td.children[0].text.to_i,
+          name: td.children[3].text,
+          user_id: td.children[3].attr('href').split('/').last.to_i,
+          point: td.children[7].text.delete(',').to_i,
+        }
+      end
     end
 
     def ula_ranking(event_id, upto, team_no)
